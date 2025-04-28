3b1af78aa1c1e98834df2a71ce17a45810ef5126
hyerrim11 <hyerim11@sookmyung.ac.kr>
2025-04-28 10:59:03 +0900

    3주차 리액트 스터디

---
+++ 이혜림/section3/src/math.js
@@ -0,0 +1,26 @@
+// math 모듈
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// function sub(a, b) {
+//   return a - b;
+// }
+
+// module.exports = {
+//   add,
+//   sub,
+// };
+//math module
+
+export function add(a, b) {
+  return a + b;
+}
+export function sub(a, b) {
+  return a - b;
+}
+
+export default function multiply(a, b) {
+  return a + b;
+}
