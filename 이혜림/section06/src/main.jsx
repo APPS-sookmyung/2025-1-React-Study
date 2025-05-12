4eb01ab087b6b8d70f19cc5214f8bf095da8aa65
hyerrim11 <hyerim11@sookmyung.ac.kr>
2025-05-12 21:17:51 +0900

    Feat: 5주차 과제 및 실습코드

---
+++ 이혜림/section06/src/main.jsx
@@ -0,0 +1,6 @@
+import React from "react"; // React를 명시적으로 임포트
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
